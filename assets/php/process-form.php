e8d8d89dc9f4682b654c2710569df8abe4b805c7
Valentino Trenchev <74861035+ValentinoTrenchev@users.noreply.github.com>
2023-11-26 23:42:19 +0100

    Add files via upload

--- assets/php/process-form.php
+++ assets/php/process-form.php
@@ -1,51 +1,39 @@
-<?php 
-$errors = '';
-$myemail = 'valentino@trencev.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
 <?php
-echo nl2br($errors);
+
+if (isset($_REQUEST['name'],$_REQUEST['surname'],$_REQUEST['email'])) {
+
+      
+
+    $name = $_REQUEST['name'];
+
+    $surname = $_REQUEST['surname']
+
+    $mail = $_REQUEST['email'];
+
+    $message = $_REQUEST['comment'];
+
+      
+
+    // Set your email address where you want to receive emails. 
+
+    $to = 'valentino@trencev.com';
+
+      
+
+    $subject = 'Contact From My Website';
+
+    $headers = "From: ".$name." <".$mail."> \r\n";
+
+      
+
+    $send_email = mail($to,$subject,$message,$headers);
+
+      
+
+    echo ($send_email) ? 'success' : 'error';
+
+      
+
+}
+
 ?>
-
-
-</body>
-</html>
